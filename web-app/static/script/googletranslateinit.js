e6ff566d59faa0e7027699b1c9ea60ce3e635d72
arya-rgb <dickyarya.personal@gmail.com>
2021-12-19 01:02:54 +0700

    add translator system
    using google translate widget integrated to website

---
+++ web-app/static/script/googletranslateinit.js
@@ -0,0 +1,6 @@
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({
+      pageLanguage: 'id',
+      layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+    }, 'google_translate_element');
+  }
